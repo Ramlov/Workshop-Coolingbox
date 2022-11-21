15b70f57e6f95e793eed91419bee75f7f76407e4
Ramlov <mathias.ramlov10@gmail.com>
2022-11-21 08:34:41 +0100

    done

--- helper_cls.py
+++ helper_cls.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from math import e
 
 
 class Cooler:
@@ -11,9 +11,11 @@ class Cooler:
     def simple_thermostat(self, Tdegrees: int) -> bool:
         return True if Tdegrees > 5 else False
 
-    def compressor(self, Tdegrees: int, Tperiode: int):
-        if Cooler.simple_thermostat(self, Tdegrees):
+    def compressor(self, Tdegrees: int, Tperiode: int, df, Current_N: int):
+        if Cooler.ai_thermostat(self, Current_N, df):
             return 8*10**-6*Tperiode**-1
+       # if Cooler.simple_thermostat(self, Tdegrees):
+       #     return 8*10**-6*Tperiode**-1
         else:
             return 0*Tperiode**-1
 
@@ -21,5 +23,15 @@ class Cooler:
         df_new = df.loc[df.index[Current_N], 'Pris']
         return df_new
 
-    def ai_thermostat(self):
-        pass
+    def ai_thermostat(self, Current_N: int, df):
+        if float(df.loc[df.index[Current_N-1], 'Pris']) < float(df.loc[df.index[Current_N], 'Pris']):
+            return True
+
+    def food_waste(self, Tdegress: int):
+        if Tdegress < 3.5:
+            return 4.39*e**-0.49*Tdegress
+        elif 3.5 <= Tdegress < 6.5:
+            return 0
+        elif Tdegress > 6.5:
+            return 0.11*e**0.31*Tdegress
+        
--- main.py
+++ main.py
@@ -22,37 +22,63 @@ class Resturant():
     Toutside: int = 20
     Tkomp: int = -5
     Total_cost: list = []
+    Total_expence: list = []
+    Total_expence_sum: list = []
+
 
     def __init__(self) -> None:
         super(Cooler)
         self.df = pd.read_csv("elpris.csv")
         
-    def main(self):
+    def __call__(self):
 
         while len(self.N) < 8640: #Intervaller = 8640
-            self.Tdegrees = self.Tdegrees + (Cooler.door(self, self.Tperiode)*(self.Toutside-self.Tdegrees)+Cooler.compressor(self, self.Tdegrees, self.Tperiode)*(self.Tkomp-self.Tdegrees))*self.Tperiode
+            self.Tdegrees = self.Tdegrees + (Cooler.door(self, self.Tperiode)*(self.Toutside-self.Tdegrees)+Cooler.compressor(self, self.Tdegrees, self.Tperiode, self.df, len(self.N))*(self.Tkomp-self.Tdegrees))*self.Tperiode
 
-            if Cooler.compressor(self, self.Tdegrees, self.Tperiode) != 0.0:
+            if Cooler.compressor(self, self.Tdegrees, self.Tperiode, self.df, len(self.N)) != 0.0:
                 self.Total_cost.append(Cooler.calc_usage(self, len(self.N), self.df))
+            else:
+                self.Total_cost.append(0)
             self.N.append(self.Tdegrees)
 
+            # Total udgift Monte Carlo
+            expense = (self.Total_cost[-1]+Cooler.food_waste(self, self.N[-1]))
+            self.Total_expence.append(expense)
+            self.Total_expence_sum.append(sum(self.Total_expence)/len(self.N)) # 
+        
+
         #print(self.N[np.argmax(self.N)])
-        print(f'Gennemsnitteligt forbrug i september {sum(self.Total_cost)/30: 10} DKK') #Gennemsnittelig pris
-        print(f'Bugettet et overholdt? {sum(self.Total_cost) < 1200}')       
+        print(f'Gennemsnitteligt strøm forbrug i september {sum(self.Total_cost)/30: 10} DKK') #Gennemsnittelig pris
+        print(f'Bugettet et overholdt? {sum(self.Total_cost) < 12000}')       
        
-        plt.plot(self.N, label='Temp') #Plot temp
-        plt.legend()
+        print(f'Summen af alle udgifter, madspild+strømforbrug: {sum(self.Total_expence)}')
+
+        fig, ax = plt.subplots(3)
+
+            # For Sine Function
+        ax[0].plot(self.N)
+        ax[0].set_title("Temp")
+
+        # For Cosine Function
+        ax[1].plot(self.Total_cost)
+        ax[1].set_title("Cost")
+
+        # For Tangent Function
+        ax[2].plot(self.Total_expence_sum)
+        ax[2].set_title("Monte Carlo Sum")
+        #plt.plot(self.Total_expence_sum)
+        #plt.plot(self.N, label='Temp') #Plot temp
+        #plt.legend()
         plt.show()
         
-        plt.plot(self.Total_cost, label='Cost') #Plot cost
-        plt.legend()
-        plt.show()
+        #plt.plot(self.Total_cost, label='Cost') #Plot cost
+        #plt.legend()
+        #plt.show()
 
 
 if __name__ == '__main__':
     '''Doctest module'''
-    test = Resturant()
-    test.main()
-
+    rest = Resturant()
+    rest()    
 
 
