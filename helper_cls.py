1fd5b6df5789b1f49a6755e72f0a92aede1dd09f
Ramlov <mathias.ramlov10@gmail.com>
2022-12-01 18:15:49 +0100

    done

--- helper_cls.py
+++ helper_cls.py
@@ -1,33 +1,53 @@
+'''
+Fil der indeholder alle helper_cls
+
+'''
+
 import numpy as np
 from math import e
 
+
+useSmart = True #Sæt til False for Simple, eller True for smart thermostat
 
 class Cooler:
     totalcost = 0
 
     def door(self, Tperiode):
+        '''Functionen her returnere temperatur udviklingen afhængigt af om døren er åben eller ikke'''
         return 3*10**-5*Tperiode**-1 if np.random.randint(0, 100) %10 == 0 else 5*10**-7*Tperiode**-1
 
     def simple_thermostat(self, Tdegrees: int) -> bool:
+        '''Simple termostat'''
         return True if Tdegrees > 5 else False
 
     def compressor(self, Tdegrees: int, Tperiode: int, df, Current_N: int):
-        if Cooler.ai_thermostat(self, Current_N, df):
-            return 8*10**-6*Tperiode**-1
-       # if Cooler.simple_thermostat(self, Tdegrees):
-       #     return 8*10**-6*Tperiode**-1
+        '''Kompresseren der returnere hvordan temperaturudviklingen skal håndteres'''
+        if useSmart:
+            if Cooler.ai_thermostat(self, Current_N, df):
+                return 8*10**-6*Tperiode**-1
+            else:
+                if Tdegrees > 6.5:
+                    return 8*10**-6*Tperiode**-1
+                else:
+                    return 0*Tperiode**-1
         else:
-            return 0*Tperiode**-1
+            if Cooler.simple_thermostat(self, Tdegrees):
+                return 8*10**-6*Tperiode**-1
+            else:
+                return 0*Tperiode**-1
 
     def calc_usage(self, Current_N: int, df):
+        '''Her beregnes prisen ved brug af df loaded i main.'''
         df_new = df.loc[df.index[Current_N], 'Pris']
         return df_new
 
     def ai_thermostat(self, Current_N: int, df):
+        '''Her tjekkes der for om det er billigere at vente med at køle.'''
         if float(df.loc[df.index[Current_N-1], 'Pris']) < float(df.loc[df.index[Current_N], 'Pris']):
             return True
 
     def food_waste(self, Tdegress: int):
+        '''Her beregnes foodwaste, hvis temperaturen er under 3.5 eller over 6.5'''
         if Tdegress < 3.5:
             return 4.39*e**-0.49*Tdegress
         elif 3.5 <= Tdegress < 6.5:
--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-''''@author Ramlov
-      ___            _         
-     | _ \__ _ _ __ | |_____ __
-     |   / _` | '  \| / _ \ V /
-     |_|_\__,_|_|_|_|_\___/\_/ 
+'''
+Main file til loop af funktionen.
 
 '''
 
@@ -15,6 +12,7 @@ import pandas as pd
 
 
 class Resturant():
+    '''Her er variablerne der bruges i class'en gemt'''
     Tdegrees: float = 5. #Temp 5 grader
     N: list = [] #Nuværende interval
     n: int = 1
@@ -27,11 +25,13 @@ class Resturant():
 
 
     def __init__(self) -> None:
+        '''init functionen bruges til at loade dataframe(elpriser.csv) og bruger super(Cooler)
+        til at loade alle functionerne fra cooler classen herind.'''
         super(Cooler)
         self.df = pd.read_csv("elpris.csv")
         
-    def __call__(self):
-
+    def main(self):
+        '''Call functionen bruges til at køre scriptet når der bliver lavet et attribute til classen'''
         while len(self.N) < 8640: #Intervaller = 8640
             self.Tdegrees = self.Tdegrees + (Cooler.door(self, self.Tperiode)*(self.Toutside-self.Tdegrees)+Cooler.compressor(self, self.Tdegrees, self.Tperiode, self.df, len(self.N))*(self.Tkomp-self.Tdegrees))*self.Tperiode
 
@@ -53,23 +53,29 @@ class Resturant():
        
         print(f'Summen af alle udgifter, madspild+strømforbrug: {sum(self.Total_expence)}')
 
-        fig, ax = plt.subplots(3)
+        print(f'Din mor {sum(self.Total_expence)}')
 
-            # For Sine Function
-        ax[0].plot(self.N)
-        ax[0].set_title("Temp")
+        #fig, ax = plt.subplots(3)
 
-        # For Cosine Function
-        ax[1].plot(self.Total_cost)
-        ax[1].set_title("Cost")
+        #ax[0].plot(self.N)
+        #ax[0].set_title("Temp")
 
-        # For Tangent Function
-        ax[2].plot(self.Total_expence_sum)
-        ax[2].set_title("Monte Carlo Sum")
-        #plt.plot(self.Total_expence_sum)
-        #plt.plot(self.N, label='Temp') #Plot temp
-        #plt.legend()
+        #ax[1].plot(self.Total_cost)
+        #ax[1].set_title("Cost")
+
+        #ax[2].plot(self.Total_expence_sum)
+        #ax[2].set_title("Monte Carlo Sum")
+
+        plt.plot(self.Total_cost, label="Cost")
         plt.show()
+        plt.legend()
+        plt.plot(self.Total_expence_sum, label='Mote Carlo Sum')
+        plt.show()
+        plt.legend()
+        plt.plot(self.N, label='Temp') #Plot temp
+        plt.legend()
+        plt.show()
+        
         
         #plt.plot(self.Total_cost, label='Cost') #Plot cost
         #plt.legend()
@@ -79,6 +85,6 @@ class Resturant():
 if __name__ == '__main__':
     '''Doctest module'''
     rest = Resturant()
-    rest()    
+    rest.main()    
 
 
